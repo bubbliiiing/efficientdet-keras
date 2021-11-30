4cb9989464282255e95cf0e08064e2b49a80429e
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-11-30 19:20:47 +0800

    Update summary.py

--- summary.py
+++ summary.py
@@ -2,12 +2,12 @@
 #   该部分代码用于看网络结构
 #--------------------------------------------#
 from nets.efficientdet import efficientdet
-from utils.utils import get_classes, image_sizes
+from utils.utils import image_sizes
 
 if __name__ == "__main__":
     num_classes = 20
     phi         = 0
-    input_shape = [image_sizes[phi], image_sizes[phi]]
+    input_shape = [image_sizes[phi], image_sizes[phi], 3]
 
     model = efficientdet(input_shape, phi, num_classes)
     model.summary()
