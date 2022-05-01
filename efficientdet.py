78d0eb639c22aadbb1aed34b01b9b3a8f4473814
Bubbliiiing <3323290568@qq.com>
2022-05-01 19:59:21 +0800

    update best checkpoint and config show

--- efficientdet.py
+++ efficientdet.py
@@ -8,7 +8,7 @@ from PIL import ImageDraw, ImageFont
 from nets.efficientdet import efficientdet
 from utils.anchors import get_anchors
 from utils.utils import (cvtColor, get_classes, image_sizes, preprocess_input,
-                         resize_image)
+                         resize_image, show_config)
 from utils.utils_bbox import BBoxUtility
 
 '''
@@ -61,6 +61,7 @@ class Efficientdet(object):
     #---------------------------------------------------#
     def __init__(self, **kwargs):
         self.__dict__.update(self._defaults)
+        show_config(**self._defaults)
         for name, value in kwargs.items():
             setattr(self, name, value)
         self.input_shape                    = [image_sizes[self.phi], image_sizes[self.phi]]
