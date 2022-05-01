a5c0da11552175e3b17bd0a7c9a57a26a105f3f0
Bubbliiiing <3323290568@qq.com>
2022-05-01 14:14:37 +0800

    update flops and training steps

--- summary.py
+++ summary.py
@@ -2,15 +2,25 @@
 #   该部分代码用于看网络结构
 #--------------------------------------------#
 from nets.efficientdet import efficientdet
-from utils.utils import image_sizes
+from utils.utils import image_sizes, net_flops
 
 if __name__ == "__main__":
-    num_classes = 20
     phi         = 0
     input_shape = [image_sizes[phi], image_sizes[phi], 3]
+    num_classes = 80
 
     model = efficientdet(input_shape, phi, num_classes)
+    #--------------------------------------------#
+    #   查看网络结构网络结构
+    #--------------------------------------------#
     model.summary()
-
+    #--------------------------------------------#
+    #   计算网络的FLOPS
+    #--------------------------------------------#
+    net_flops(model, table=False)
+    
+    #--------------------------------------------#
+    #   获得网络每个层的名称与序号
+    #--------------------------------------------#
     # for i,layer in enumerate(model.layers):
     #     print(i,layer.name)
